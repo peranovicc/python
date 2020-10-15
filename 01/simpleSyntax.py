9dfe39553029685a377991a8027342d2e3b0ca29
peranovicc <cvijanperanovic@aol.com>
2020-10-15 12:42:06 +0200

    Simple syntax

---
+++ 01/simpleSyntax.py
@@ -0,0 +1,12 @@
+print("Hello")
+
+x = 5
+
+if x < 6: 
+    print('x is less than 6') # Important! : Indentation acts as code block: {} in some languages 
+    # some commands
+
+else:
+    print('x is not less than 6')
+
+
