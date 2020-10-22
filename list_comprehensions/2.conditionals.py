dfc1135f933157ba33538cbadc28579704c3811b
peranovicc <cvijanperanovic@aol.com>
2020-10-22 22:35:46 +0200

    Conditional in list comprehension

--- list_comprehensions/2.conditionals.py
+++ list_comprehensions/2.conditionals.py
@@ -1,8 +1,11 @@
 # filter and map
 lista = [10,221,53,31,231,2131,21,312,1,13,4,5]
+lista2 = ['Pera','Mika','Zika']
 
 result = [x * 2 if x % 2 == 0 else x + 1 for x in lista if x >= 10]
+result2 = [(x,y) for x in lista if x > 55 for y in lista2]
 # left side of loop - mapping with condition
 # right side of loop - filtering 
 
 print(result) # [20, 222, 54, 32, 232, 2132, 22, 624, 14]
+print(result2)
