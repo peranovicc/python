a67f4c716f95c5bec3d3e4489a58e4c6a24d4c95
peranovicc <cvijanperanovic@aol.com>
2020-10-24 23:02:07 +0200

    Removed tqdm

--- Algorithmic-problems/2.binary_search.py
+++ Algorithmic-problems/2.binary_search.py
@@ -1,15 +1,13 @@
-from tqdm import tqdm
 
-def binary_search(arr,low,high,k,bar):
-    bar.update(1)
+def binary_search(arr,low,high,k):
     if low <= high:
         mid = (low + high) // 2
         if arr[mid] == k:
             return mid + 1
         if arr[mid] < k:
-            return binary_search(arr,mid+1,high,k,bar)
+            return binary_search(arr,mid+1,high,ks)
         if arr[mid] > k:
-            return binary_search(arr,low,mid-1,k,bar)
+            return binary_search(arr,low,mid-1,k)
     
     else:
         return -1
@@ -17,10 +15,9 @@ def binary_search(arr,low,high,k,bar):
 fi = open('binary.txt','r')
 
 n = int(fi.readline())
-bar = tqdm(total = n)
 m = int(fi.readline())
 arr = [int(x) for x in fi.readline().split()]
 ks = [int(x) for x in fi.readline().split()]
 
 fo = open('binaryOut.txt','w')
-fo.write(' '.join(str(binary_search(arr,0,len(arr)-1,k,bar)) for k in ks))
+fo.write(' '.join(str(binary_search(arr,0,len(arr)-1,k)) for k in ks))
